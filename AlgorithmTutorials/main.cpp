34ed25bec57225ddaa3e6da5b4797f29b1eb8d47
Vishal <Vishal.Patel@nab.com.au>
2013-06-24 23:12:44 +1000

    Initial Commit

---
+++ AlgorithmTutorials/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AlgorithmTutorials
+//
+//  Created by Vishal on 24/06/13.
+//  Copyright (c) 2013 Vishal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
