c959b84bae9b730b7b104ac6bef068f1190f9cb2
Vishal Patel <vis8051@gamil.com>
2014-01-12 20:41:59 +1100

    [KMP string seraching algorithm added with O(n) complexity

---
+++ AlgorithmTutorials/KMP.cpp
@@ -0,0 +1,20 @@
+//
+//  KMP.cpp
+//  AlgorithmTutorials
+//
+//  Created by Vishal Patel on 1/12/14.
+//  Copyright (c) 2014 Vishal. All rights reserved.
+//
+
+#include "KMP.h"
+
+
+KMP::KMP()
+{
+    
+}
+
+void KMP::run()
+{
+    
+}
---
+++ AlgorithmTutorials/KMP.h
@@ -0,0 +1,27 @@
+//
+//  KMP.h
+//  AlgorithmTutorials
+//
+//  Created by Vishal Patel on 1/12/14.
+//  Copyright (c) 2014 Vishal. All rights reserved.
+//
+
+#ifndef __AlgorithmTutorials__KMP__
+#define __AlgorithmTutorials__KMP__
+
+#include <iostream>
+#include "TutorialBase.h"
+
+// substring algorithm with O(n) complexity.
+
+class KMP : public TutorialBase {
+    
+    
+public:
+    KMP();
+    virtual void run();
+};
+
+#endif /* defined(__AlgorithmTutorials__SpiralMatrix__) */
+
+
