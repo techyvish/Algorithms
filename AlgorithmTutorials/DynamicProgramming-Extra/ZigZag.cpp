37f4cd2372cd855cfa34b0b9db173b05bbaed456
Vishal <Vishal.Patel@nab.com.au>
2014-07-08 17:02:03 +1000

    Zigzag completed

--- AlgorithmTutorials/DynamicProgramming-Extra/ZigZag.cpp
+++ AlgorithmTutorials/DynamicProgramming-Extra/ZigZag.cpp
@@ -104,12 +104,13 @@ struct element
 class ZigZag {
     
     vector <element> v;
-    
+
 public:
+
+    int length;
     
     int  longestZigZag(int sequence[])
     {
-        int length = 6;
         element e;
         e.lzValue = 1;
         e.status = START;
@@ -162,17 +163,26 @@ public:
             }
             
             v.push_back(selected);
+            cout << "-----------------" << endl;
         }
         
-        return 0;
+        return (*it).lzValue;
     }
 };
 
 int main()
 {
-    
-    int seq[] = { 1, 7, 4, 9, 2, 5 };
+    //int seq[] = { 1, 7, 4, 9, 2, 5 };
+    //int seq[] = { 1, 17, 5, 10, 13, 15, 10, 5, 16, 8 };
+    //int seq[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    //int seq[] = {70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32};
+    int seq[] = {374, 40, 854, 203, 203, 156, 362, 279, 812, 955,
+                 600, 947, 978, 46, 100, 953, 670, 862, 568, 188,
+                 67, 669, 810, 704, 52, 861, 49, 640, 370, 908,
+                 477, 245, 413, 109, 659, 401, 483, 308, 609, 120,
+                 249, 22, 176, 279, 23, 22, 617, 462, 459, 244 };
     ZigZag z;
+    z.length = sizeof(seq)/sizeof(seq[0]);
     int length = z.longestZigZag(seq);
     cout << length;
     return 0;
