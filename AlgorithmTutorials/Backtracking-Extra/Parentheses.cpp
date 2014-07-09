d796b86739175da6465572a8e8ce8e1879245b7b
Vishal <Vishal.Patel@nab.com.au>
2014-07-10 08:35:50 +1000

    DP - Bad Neighbour Backtracking - Parenthesis

---
+++ AlgorithmTutorials/Backtracking-Extra/Parentheses.cpp
@@ -0,0 +1,57 @@
+//
+//  Parentheses.cpp
+//  AlgorithmTutorials
+//
+//  Created by Vishal Patel on 9/07/2014.
+//  Copyright (c) 2014 Vishal. All rights reserved.
+//
+#include <cstdio>
+
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+#include <unistd.h>
+#include <climits>
+
+using namespace std;
+
+class Parentheses {
+
+public:
+    int n ;
+    
+    void printParenthesis(int i, int j )
+    {
+        if ( j == n )
+            return ;
+        
+        while ( i < n) {
+            cout << "(";
+            printParenthesis(++i,j + 1);
+            i++;
+            cout << ")";
+            printParenthesis(++i,j + 1);
+            
+            //cout << ")";
+        }
+    }
+};
+
+int main()
+{
+    Parentheses p;
+    p.n = 2;
+    for ( int j = 0 ; j < 2 ; j ++ )
+    {
+        p.printParenthesis( 0 , 0 );
+        cout << endl << "------" << endl ;
+    }
+    return 0;
+}
---
+++ AlgorithmTutorials/DynamicProgramming-Extra/BadNeighbours.cpp
@@ -0,0 +1,54 @@
+//
+//  BadNeighbours.cpp
+//  AlgorithmTutorials
+//
+//  Created by Vishal Patel on 8/07/2014.
+//  Copyright (c) 2014 Vishal. All rights reserved.
+//
+
+#include <cstdio>
+
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+#include <unistd.h>
+#include <climits>
+
+using namespace std;
+
+class BadNeighbors {
+    
+    
+public:
+    
+    int length ;
+    
+    int maxDonations(int arr[])
+    {
+        return 0;
+    }
+    
+};
+
+//int main()
+//{
+//    int a[] = {10, 3, 2, 5, 7, 8};
+//    int b[]= {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+//    int c[]= {94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61, 6, 237,
+//        12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397, 52, 72, 37, 51, 1, 81,
+//        45, 435, 7, 36, 57, 86, 81, 72};
+//    int d[]={7, 7, 7, 7, 7, 7, 7};
+//
+//    BadNeighbors bn;
+//    bn.length = sizeof(a)/sizeof(a[0]);
+//    int donation = bn.maxDonations(a);
+//    cout << donation << endl;
+//    return 0;
+//} 
