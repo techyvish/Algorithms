f1eaed80619f52f75fcb73cdc16869d0aadfa004
Vishal <Vishal.Patel@nab.com.au>
2014-07-16 12:56:53 +1000

    N Queens problem. (Generalised backtrack method ).

--- AlgorithmTutorials/Backtracking-Extra/Queens.cpp
+++ AlgorithmTutorials/Backtracking-Extra/Queens.cpp
@@ -21,6 +21,24 @@
 #include <unistd.h>
 #include <climits>
 
+/**
+ *  Place N Queens on chess board so that they can not threat each other
+ *  horizontally or vertically or diagonally.
+ *
+ *   O  X  O  O
+ *   O  O  O  X
+ *   X  O  O  O
+ *   O  O  X  O
+ *
+ *  This program uses generalised backtrack solution from template.
+ *  This program is to prove that backtrack general solution works.
+ *
+ *  Concept: we'll carefully bruteforce to findout all posible position
+ *  we can place queen on a chess board. we'll then if one perticular
+ *  solution doesn't work then we'll backtrack and fix the problem.
+ *
+ **/
+
 #define NMAX 10
 
 using namespace std;
@@ -33,88 +51,94 @@ struct _data
 typedef struct _data data;
 
 class Queens {
-    
-    
+
 public:
     
     bool finished = false;
     
     bool is_a_solution(int a[][NMAX],int k, int n)
     {
-        return ( k == n );
+        return ( k == n - 1);
     }
     
-    void process_solution(int a[][NMAX],int k , int n)
+    void process_solution(int a[][NMAX],int k , int n,int *x)
     {
-    }
-    
-    void construct_candidates(int a[][NMAX],int k, int n, int c[], int *ncandidates)
-    {
-        for (int i = 0 ; i < n ; i++ )
+        for ( int i = 0 ; i < n ; i ++ )
         {
-            if ( canQueenBePlaced(a,k,i,n) )
+            for ( int j = 0 ; j < n ; j ++ )
             {
-                //Erase old position (if any) and place the queen at new position.
-                
-                
-                
-                //and count remaining candidates for future.
-                *ncandidates = *ncandidates + 1;
+                if ( x[i] == j )
+                {
+                    cout << " X ";
+                }
+                else
+                {
+                    cout << " O ";
+                }
             }
-            else
+            cout << endl;
+        }
+        cout << endl;
+        finished = true;
+    }
+    
+    void construct_candidates(int a[][NMAX],int k, int n, int c[], int *ncandidates,int x[])
+    {
+        cout << endl;
+        for ( int col = 0 ; col < n ; col ++) // for each column
+        {
+            bool leagal_move = true;
+            for ( int row = 0 ; row < k ; row++ ) // we'll examine each row to find out wether queen can be placed or not.
             {
-                *ncandidates = 0 ; // no candidates left to try -> backtrack.
+                if ( x[row] == col )   // is it in same column ??
+                    leagal_move = false;
+                if ( abs(col - x[row] ) == abs( k - row ) )  // check difference of columns and rows is equal for diagonal threat.
+                    leagal_move = false;
+            }
+            if ( leagal_move == true )
+            {
+                c[*ncandidates] = col;  // if we can place queen then add remaining positions where queens can be paced
+                                        // for future use.
+                *ncandidates = *ncandidates + 1;
             }
         }
     }
     
-    void backtrack(int a[][NMAX],int k , int  n)
+    void backtrack(int a[][NMAX],int k , int  n,int *x)
     {
-        int c[3];
-        int ncandidates;
-        int i ;
+        int *c = new int[NMAX]; //final solution vector
+        std::fill_n(c, NMAX, 0);
+        
+        int ncandidates = 0;
+        int i = 0;
         
         if ( is_a_solution(a,k,n))
-            process_solution(a,k,n);
+            process_solution(a,k,n,x);
         else
         {
             k = k+1; // rows
-            construct_candidates(a,k,n,c,&ncandidates);
-            for ( i = 0 ; i < ncandidates ; i++ )
-            {
+            construct_candidates(a,k,n,c,&ncandidates,x);
+            for (  i =0 ; i < ncandidates ; i++ )      //TODO: find out where i > ncadidates that mean we've to remove item
+            {                                          // from last column and back track
                 a[k][i] = c[i];
-                backtrack(a,k,n);
+                x[k] = c[i];  // store queen position in final solution vector.
+                //cout << "Can we place queen at row " << k << "  and column " << c[i] << "?" << endl;
+                backtrack(a,k,n,x);
                 if ( finished )
                     return;
             }
+            
+            delete c; // clean up
         }
     }
     
     void planceQueens(int n )
     {
-        int a[NMAX][NMAX];  /* solution vector */
-        backtrack(a,0,n);
-    }
-    
-    bool canQueenBePlaced(int a[][NMAX],int row, int col,int n)
-    {
-        for (int i = 0 ; i < row ; i++ )
-        {
-            for ( int j = 0 ; j < col ; j++ )
-            {
-                if ( a[i][j] == 1 )
-                {
-                    if ( row == i || col == j)
-                        return false;
-                }
-                
-                if ( a[abs(i - row)][abs(j-col)] == 1 )
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
+        int a[NMAX][NMAX] = {0};  /* solution vector */
+        int *x = new int[NMAX];
+        std::fill_n(x, NMAX, INT_MAX);
+        backtrack(a,-1,n,x);
+        delete x; // clean up;
     }
     
 };
@@ -122,6 +146,6 @@ public:
 int main()
 {
     Queens q;
-    q.planceQueens(8);
+    q.planceQueens(4);
     return 0;
 }
