b97bb2e1531af9773d2b4aaec7313a518d91636c
Vishal <Vishal.Patel@nab.com.au>
2014-07-21 09:08:47 +1000

    Greedy Algorithms. SRMS.

--- AlgorithmTutorials/TopCoder/SRM 573/SkiResortsEasy.cpp
+++ AlgorithmTutorials/TopCoder/SRM 573/SkiResortsEasy.cpp
@@ -12,81 +12,27 @@
 
 using namespace std;
 
-struct data {
-    int min;
-    int realData;
-public:
-
-};
-
-bool sortthisshit(data const& a, data const& b)  {
-    if ( a.min < b.min)
-        return true;
-    else
-        return false;
-}
-
-class CostOfDancing {
+class SkiResortsEasy {
     public:
-    
-    vector<data> minCost;
-    int minimum(int K, vector<int> danceCost) {
-        sort(danceCost.begin(), danceCost.end());
-        int sum = 0 ;
-        for (int i = 0 ; i < K ; i ++ )
+    int minCost(vector<int> altitude) {
+        int cost = 0;
+        for ( int i = 0 ; i < altitude.size(); i++ )
         {
-            sum += danceCost[i];
+            if ( i!= 0 )
+            {
+                if ( altitude[i] > altitude[i-1] ) {
+                    cost += altitude[i] - altitude[i-1];
+                    altitude[i] = altitude[i-1];
+                }
+            }
         }
-        return sum;
+        return cost;
     }
-    
-    
-//    int minimum1(int K, vector<int> danceCost) {
-//        
-//        for ( int i = 0 ; i < danceCost.size() ; i ++ )
-//        {
-//            int min = INT_MAX;
-//            bool found = false;
-//            data d;
-//            for (int  j = 0 ; j < i ; j ++ )
-//            {
-//                if ( (danceCost[i] + danceCost[j]) < min)
-//                {
-//                    min = (danceCost[i] + danceCost[j]);
-//                    found = true;
-////                    d.min = min;
-////                    d.realData = danceCost[i];
-//                }
-//            }
-//            if ( !found && i!=0) {
-//                min = danceCost[i] + danceCost[i-1];
-//                d.min = min;
-//                d.realData = danceCost[i];
-//                
-//            }
-//            if ( i == 0 )
-//            {
-//                min = danceCost[0];
-//                d.min = min;
-//                d.realData = danceCost[i];
-//            }
-//            d.min = min;
-//            d.realData = danceCost[i];
-//            minCost.push_back(d);
-//        }
-//        
-//        std::sort(minCost.begin(), minCost.end(), sortthisshit);
-//        int sum = 0;
-//        for ( int i = 0 ; i < K ; i++ )
-//        {
-//            sum += minCost[i].realData;
-//        }
-//        return sum;
-//    }
 };
 
 //// CUT begin
-//ifstream data("/Users/vishal 1/Algorithm/AlgorithmTutorials/TopCoder/SRM 624/CostOfDancing.sample");
+//ifstream data("/Users/vishal 1/Algorithm/AlgorithmTutorials/TopCoder/SRM 573/SkiResortsEasy.sample");
+////ifstream data("SkiResortsEasy.sample");
 //
 //string next_line() {
 //    string s;
@@ -125,10 +71,10 @@ class CostOfDancing {
 //    return "\"" + t + "\"";
 //}
 //
-//bool do_test(int K, vector<int> danceCost, int __expected) {
+//bool do_test(vector<int> altitude, int __expected) {
 //    time_t startClock = clock();
-//    CostOfDancing *instance = new CostOfDancing();
-//    int __result = instance->minimum(K, danceCost);
+//    SkiResortsEasy *instance = new SkiResortsEasy();
+//    int __result = instance->minCost(altitude);
 //    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
 //    delete instance;
 //
@@ -149,10 +95,8 @@ class CostOfDancing {
 //    while (true) {
 //        if (next_line().find("--") != 0)
 //            break;
-//        int K;
-//        from_stream(K);
-//        vector<int> danceCost;
-//        from_stream(danceCost);
+//        vector<int> altitude;
+//        from_stream(altitude);
 //        next_line();
 //        int __answer;
 //        from_stream(__answer);
@@ -162,13 +106,13 @@ class CostOfDancing {
 //            continue;
 //
 //        cout << "  Testcase #" << cases - 1 << " ... ";
-//        if ( do_test(K, danceCost, __answer)) {
+//        if ( do_test(altitude, __answer)) {
 //            passed++;
 //        }
 //    }
 //    if (mainProcess) {
 //        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
-//        int T = time(NULL) - 1405657719;
+//        int T = time(NULL) - 1405804727;
 //        double PT = T / 60.0, TT = 75.0;
 //        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
 //        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
@@ -189,7 +133,7 @@ class CostOfDancing {
 //        }
 //    }
 //    if (mainProcess) {
-//        cout << "CostOfDancing (250 Points)" << endl << endl;
+//        cout << "SkiResortsEasy (250 Points)" << endl << endl;
 //    }
 //    return run_test(mainProcess, cases, argv[0]);
 //}
