360afb0057cde6f7075a4ce60b750034f184393e
Vishal <vishal.x.patel@nab.com.au>
2014-08-20 14:57:25 +1000

    SRM 158 Div2 L1 [Iteration]

---
+++ Python/TopCoderProblems/SRM 158/TireRotation.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+import math,string,itertools,fractions,heapq,collections,re,array,bisect
+
+class TireRotation:
+    def getCycle(self, initial, current):
+        print("Start");
+        firstSwap ="";
+        secondSwap ="";
+        thirdSwap ="";
+        forthSwap ="";
+
+        firstSwap = initial[3] + initial[2] + initial[0] + initial[1];
+        secondSwap = firstSwap[3] + firstSwap[2] + firstSwap[0] + firstSwap[1];
+        thirdSwap = secondSwap[3] + secondSwap[2] + secondSwap[0] + secondSwap[1];
+        forthSwap = thirdSwap[3] + thirdSwap[2] + thirdSwap[0] + thirdSwap[1];
+
+        if current == initial:
+            return 1;
+
+        if current == firstSwap:
+            return 2;
+
+        if current == secondSwap:
+            return 3;
+
+        if current == thirdSwap:
+            return 4;
+
+        return -1;
+
+# CUT begin
+# TEST CODE FOR PYTHON {{{
+import sys, time, math
+
+def tc_equal(expected, received):
+    try:
+        _t = type(expected)
+        received = _t(received)
+        if _t == list or _t == tuple:
+            if len(expected) != len(received): return False
+            return all(tc_equal(e, r) for (e, r) in zip(expected, received))
+        elif _t == float:
+            eps = 1e-9
+            d = abs(received - expected)
+            return not math.isnan(received) and not math.isnan(expected) and d <= eps * max(1.0, abs(expected))
+        else:
+            return expected == received
+    except:
+        return False
+
+def pretty_str(x):
+    if type(x) == str:
+        return '"%s"' % x
+    elif type(x) == tuple:
+        return '(%s)' % (','.join( (pretty_str(y) for y in x) ) )
+    else:
+        return str(x)
+
+def do_test(initial, current, __expected):
+    startTime = time.time()
+    instance = TireRotation()
+    exception = None
+    try:
+        __result = instance.getCycle(initial, current);
+    except:
+        import traceback
+        exception = traceback.format_exc()
+    elapsed = time.time() - startTime   # in sec
+
+    if exception is not None:
+        sys.stdout.write("RUNTIME ERROR: \n")
+        sys.stdout.write(exception + "\n")
+        return 0
+
+    if tc_equal(__expected, __result):
+        sys.stdout.write("PASSED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        return 1
+    else:
+        sys.stdout.write("FAILED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        sys.stdout.write("           Expected: " + pretty_str(__expected) + "\n")
+        sys.stdout.write("           Received: " + pretty_str(__result) + "\n")
+        return 0
+
+def run_tests():
+    sys.stdout.write("TireRotation (250 Points)\n\n")
+
+    passed = cases = 0
+    case_set = set()
+    for arg in sys.argv[1:]:
+        case_set.add(int(arg))
+
+    with open("TireRotation.sample", "r") as f:
+        while True:
+            label = f.readline()
+            if not label.startswith("--"): break
+
+            initial = f.readline().rstrip()
+            current = f.readline().rstrip()
+            f.readline()
+            __answer = int(f.readline().rstrip())
+
+            cases += 1
+            if len(case_set) > 0 and (cases - 1) in case_set: continue
+            sys.stdout.write("  Testcase #%d ... " % (cases - 1))
+            passed += do_test(initial, current, __answer)
+
+    sys.stdout.write("\nPassed : %d / %d cases\n" % (passed, cases))
+
+    T = time.time() - 1408509941
+    PT, TT = (T / 60.0, 75.0)
+    points = 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+    sys.stdout.write("Time   : %d minutes %d secs\n" % (int(T/60), T%60))
+    sys.stdout.write("Score  : %.2f points\n" % points)
+
+if __name__ == '__main__':
+    run_tests()
+
+# }}}
+# CUT end
