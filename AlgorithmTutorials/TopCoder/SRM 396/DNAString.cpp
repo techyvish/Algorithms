3080b5724dcf5b86995de1a5f50cbb590b03ffd8
Vishal Patel <vis8051@gmail.com>
2015-03-12 22:51:25 +1100

    SRM 396 DIV2 L2

--- AlgorithmTutorials/TopCoder/SRM 396/DNAString.cpp
+++ AlgorithmTutorials/TopCoder/SRM 396/DNAString.cpp
@@ -101,103 +101,65 @@ template<typename T> ostream& operator <<(ostream &s, vector<T> t)
 
 using namespace std;
 
-class TaroFillingAStringDiv2 {
-public:
-    int ans =0 ;
-    char start = 0;
-    char prev = 0;
-
-    int getNumber(string s)
-    {
-        char tmp[] = {'A','B'};
-        int min = 99999;
-        for ( int i = 0 ; i < 2 ; i ++ )
-        {
-            int cnt = 0;
-            prev = tmp[i];
-            for ( int i = 0 ; i < s.length() ; i++ )
-            {
-                char c = s[i];
-                if ( c == '?')
-                {
-                    if ( prev == 'A')
-                        prev = 'B';
-                    else
-                        prev = 'A';
-                }
-                else if ( c == 'A' )
-                {
-                    if ( prev == 'A')
-                        cnt++;
-                    
-                    prev = c;
-                }
-                else if ( c == 'B')
-                {
-                    if ( prev == 'B')
-                        cnt++;
-                    prev = c;
-                }
-                
-            }
-            ans = cnt <= min ? cnt : min;
-        }
-        
-        return ans;
+class DNAString {
+    public:
+    int minChanges(int maxPeriod, vector<string> dna) {
+        return 0;
     }
 };
 
-
-/*
 // CUT begin
-ifstream data("/Users/vishal/Cerebro/Algorithms/AlgorithmTutorials/TopCoder/SRM 650/TaroFillingAStringDiv2.sample");
+ifstream data("/Users/vishal/Cerebro/Algorithms/AlgorithmTutorials/TopCoder/SRM 396/DNAString.sample");
 
-string next_line()
-{
+string next_line() {
     string s;
     getline(data, s);
     return s;
 }
 
-template <typename T> void from_stream(T &t)
-{
+template <typename T> void from_stream(T &t) {
     stringstream ss(next_line());
     ss >> t;
 }
 
-void from_stream(string &s)
-{
+void from_stream(string &s) {
     s = next_line();
 }
 
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
 template <typename T>
-string to_string(T t)
-{
+string to_string(T t) {
     stringstream s;
     s << t;
     return s.str();
 }
 
-string to_string(string t)
-{
+string to_string(string t) {
     return "\"" + t + "\"";
 }
 
-bool do_test(string S, int __expected)
-{
-    time_t                 startClock = clock();
-    TaroFillingAStringDiv2 *instance  = new TaroFillingAStringDiv2();
-    int                    __result   = instance->getNumber(S);
-    double                 elapsed    = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+bool do_test(int maxPeriod, vector<string> dna, int __expected) {
+    time_t startClock = clock();
+    DNAString *instance = new DNAString();
+    int __result = instance->minChanges(maxPeriod, dna);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
     delete instance;
 
-    if (__result == __expected)
-    {
+    if (__result == __expected) {
         cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
         return true;
     }
-    else
-    {
+    else {
         cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
         cout << "           Expected: " << to_string(__expected) << endl;
         cout << "           Received: " << to_string(__result) << endl;
@@ -205,15 +167,15 @@ bool do_test(string S, int __expected)
     }
 }
 
-int run_test(bool mainProcess, const set<int> &case_set, const string command)
-{
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
     int cases = 0, passed = 0;
-    while (true)
-    {
+    while (true) {
         if (next_line().find("--") != 0)
             break;
-        string S;
-        from_stream(S);
+        int maxPeriod;
+        from_stream(maxPeriod);
+        vector<string> dna;
+        from_stream(dna);
         next_line();
         int __answer;
         from_stream(__answer);
@@ -223,15 +185,13 @@ int run_test(bool mainProcess, const set<int> &case_set, const string command)
             continue;
 
         cout << "  Testcase #" << cases - 1 << " ... ";
-        if (do_test(S, __answer))
-        {
+        if ( do_test(maxPeriod, dna, __answer)) {
             passed++;
         }
     }
-    if (mainProcess)
-    {
+    if (mainProcess) {
         cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
-        int    T  = time(NULL) - 1425129313;
+        int T = time(NULL) - 1426160945;
         double PT = T / 60.0, TT = 75.0;
         cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
         cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
@@ -239,30 +199,21 @@ int run_test(bool mainProcess, const set<int> &case_set, const string command)
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     cout.setf(ios::fixed, ios::floatfield);
     cout.precision(2);
     set<int> cases;
-    bool     mainProcess = true;
-    for (int i = 1; i < argc; ++i)
-    {
-        if (string(argv[i]) == "-")
-        {
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
             mainProcess = false;
-        }
-        else
-        {
+        } else {
             cases.insert(atoi(argv[i]));
         }
     }
-    if (mainProcess)
-    {
-        cout << "TaroFillingAStringDiv2 (500 Points)" << endl << endl;
+    if (mainProcess) {
+        cout << "DNAString (500 Points)" << endl << endl;
     }
     return run_test(mainProcess, cases, argv[0]);
 }
-
 // CUT end
-*/
-
