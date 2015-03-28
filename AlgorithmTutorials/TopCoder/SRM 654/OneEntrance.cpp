fc9384c896fa16595e631b635b0450aacfd82c86
Vishal Patel <vis8051@gmail.com>
2015-03-28 22:49:38 +1100

    [SRM-654] D2 - L2.

--- AlgorithmTutorials/TopCoder/SRM 654/OneEntrance.cpp
+++ AlgorithmTutorials/TopCoder/SRM 654/OneEntrance.cpp
@@ -9,56 +9,66 @@
 #include <sstream>
 #include <typeinfo>
 #include <fstream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
-#define MOD 1000000007 
 
-class RockPaperScissorsMagicEasy {
+class OneEntrance{
 public:
-	long long  binormialCoeff(long long  n, long long  k)
-	{
-		long long C[100][100];
-		long long int i, j;
-		// Caculate value of Binomial Coefficient in bottom up manner 
-		for (i = 0; i <= n; i++) {
-			for (j = 0; j <= min(i, k); j++) {
-				// Base Cases 
-				if (j == 0 || j == i)
-					C[i][j] = 1;
 
-				// Calculate value using previosly stored values 
-				else
-					C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % MOD;
+
+	vector<int>g[25];
+	int dist[25];
+	bool vis[25];
+	bool valid[25];
+
+	int ar[25];
+	void dfs(int u){
+
+		vis[u] = true;
+		for (int i = 0; i<g[u].size(); i++){
+			int v = g[u][i];
+			if (valid[v] && !vis[v]){
+				dfs(v);
 			}
 		}
-		//cout<<"Hello"<<endl; 
-
-		return C[n][k];
 	}
 
-	int count(vector<int> card, int score) {
-		if (score > card.size())
-			return 0;
-		unsigned long  N = card.size();
-		long long wins = binormialCoeff(N, score);
-		long long y = 1;
-		int k = N - score;
-		long long res = 0;
-		res += wins;
-		while (k--)
-		{
-			y <<= 1;
-			y %= MOD;
+	int count(vector <int> a, vector <int> b, int s){
+		int n = (int)a.size() + 1;
+		for (int i = 0; i<a.size(); i++){
+			g[a[i]].push_back(b[i]);
+			g[b[i]].push_back(a[i]);
 		}
-		res = (res * y) % MOD;
+
+		for (int i = 0; i<n; i++){
+			ar[i] = i;
+		}
+		int res = 0;
+		do{
+			bool val = true;
+			memset(valid, true, sizeof(valid));
+			for (int i = 0; i < n; i ++)
+			{
+				cout << ar[i] << " ";
+			}
+			cout << endl;
+			for (int i = 0; i<n; i++){
+				memset(vis, false, sizeof(vis));
+				valid[ar[i]] = false;
+				dfs(ar[i]);
+				if (!vis[s]){
+					val = false;
+					break;
+				}
+			}
+			res += val;
+		} while (next_permutation(ar, ar + n));
 		return res;
 	}
 };
-/*
+
 // CUT begin
-ifstream data("SRM 653/RockPaperScissorsMagicEasy.sample");
+ifstream data("../../SRM 654/OneEntrance.sample");
 
 string next_line() {
     string s;
@@ -97,10 +107,10 @@ string to_string(string t) {
     return "\"" + t + "\"";
 }
 
-bool do_test(vector<int> card, int score, int __expected) {
+bool do_test(vector<int> a, vector<int> b, int s, int __expected) {
     time_t startClock = clock();
-    RockPaperScissorsMagicEasy *instance = new RockPaperScissorsMagicEasy();
-    int __result = instance->count(card, score);
+    OneEntrance *instance = new OneEntrance();
+    int __result = instance->count(a, b, s);
     double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
     delete instance;
 
@@ -121,10 +131,12 @@ int run_test(bool mainProcess, const set<int> &case_set, const string command) {
     while (true) {
         if (next_line().find("--") != 0)
             break;
-        vector<int> card;
-        from_stream(card);
-        int score;
-        from_stream(score);
+        vector<int> a;
+        from_stream(a);
+        vector<int> b;
+        from_stream(b);
+        int s;
+        from_stream(s);
         next_line();
         int __answer;
         from_stream(__answer);
@@ -134,13 +146,13 @@ int run_test(bool mainProcess, const set<int> &case_set, const string command) {
             continue;
 
         cout << "  Testcase #" << cases - 1 << " ... ";
-        if ( do_test(card, score, __answer)) {
+        if ( do_test(a, b, s, __answer)) {
             passed++;
         }
     }
     if (mainProcess) {
         cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
-        int T = time(NULL) - 1427004083;
+        int T = time(NULL) - 1427538546;
         double PT = T / 60.0, TT = 75.0;
         cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
         cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
@@ -161,9 +173,8 @@ int main(int argc, char *argv[]) {
         }
     }
     if (mainProcess) {
-        cout << "RockPaperScissorsMagicEasy (500 Points)" << endl << endl;
+        cout << "OneEntrance (500 Points)" << endl << endl;
     }
     return run_test(mainProcess, cases, argv[0]);
 }
 // CUT end
-*/
